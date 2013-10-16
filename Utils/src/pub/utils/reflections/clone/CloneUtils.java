7a101e3970494ffbacd4a032912b24b9cf11986e
adviou@gmail.com <adviou@gmail.com>
2013-10-16 10:51:48 -0300

    Doc

--- Utils/src/pub/utils/reflections/clone/CloneUtils.java
+++ Utils/src/pub/utils/reflections/clone/CloneUtils.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Field;
 public class CloneUtils {
 
 	/**
-	 * 
+	 * Shallow clone any object
 	 * @param Object
 	 * @return new Object
 	 * @throws InstantiationException
